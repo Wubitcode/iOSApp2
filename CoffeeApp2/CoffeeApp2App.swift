6485ef0bf03180d0a5ec88f9f46d94e4c4f9c9af
Louise <wddtechnologies@users.noreply.github.com>
2026-05-25 18:48:08 -0400

    Initial Commit

---
+++ CoffeeApp2/CoffeeApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeApp2App.swift
+//  CoffeeApp2
+//
+//  Created by Louise Rennick on 2026-05-25.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
